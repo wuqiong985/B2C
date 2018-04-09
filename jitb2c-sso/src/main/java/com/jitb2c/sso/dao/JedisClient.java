16628a5c3f38b2dc40cd126e84b1edf8f6ab84ff
wuqiong985 <709321438@qq.com>
2018-04-09 13:47:39 +0800

    1.4.1 SSO单点登录服务层环境搭建，框架整合

---
+++ jitb2c-sso/src/main/java/com/jitb2c/sso/dao/JedisClient.java
@@ -0,0 +1,49 @@
+package com.jitb2c.sso.dao;
+
+/**
+ * 将对redis的操作抽取出来封装实现
+ * @Author wuqiong6
+ * @Date 2018/3/27 17:31
+ */
+public interface JedisClient {
+
+    String get(String key);
+
+    String set(String key, String value);
+
+    String hget(String hkey, String key);
+
+    long hset(String hkey, String key, String value);
+
+    /**
+     * 自增
+     * @param key
+     * @return
+     */
+    long incr(String key);
+
+    /**
+     * 自减
+     * @param key
+     * @return
+     */
+    long decr(String key);
+
+    /**
+     * 设置有效时间
+     * @param key
+     * @param second 时间
+     * @return
+     */
+    long expire(String key, int second);
+
+    /**
+     * 获取有效时间
+     * @param key
+     * @return
+     */
+    long ttl(String key);
+
+    long del(String key);
+    long hdel(String hkey, String key);
+}
