58958b3904d72755cab8fa36be6ab468e8324650
wuqiong985 <709321438@qq.com>
2018-04-09 22:14:47 +0800

    1.4.5 SSO添加静态资源，资源映射

---
+++ jitb2c-sso/src/main/java/com/jitb2c/sso/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.jitb2c.sso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转controller
+ * @Author wuqiong6
+ * @Date 2018/4/9 22:04
+ */
+@Controller
+@RequestMapping("/page")
+public class PageController {
+
+    @RequestMapping("/register")
+    public String showRegister(){
+        return "register";
+    }
+}
